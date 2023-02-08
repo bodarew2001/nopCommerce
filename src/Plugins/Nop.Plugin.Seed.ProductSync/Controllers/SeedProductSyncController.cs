ffe83da0497c76fa920952a7769b2d4f04fdde14
Victor Bodarev <victor.bodarev.2001@gmail.com>
2023-02-08 15:51:45 +0200

    #3 Created blank plugin

---
+++ src/Plugins/Nop.Plugin.Seed.ProductSync/Controllers/SeedProductSyncController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework;
+using Nop.Web.Framework.Mvc.Filters;
+
+namespace Nop.Plugin.Seed.ProductSync.Controllers;
+
+[AutoValidateAntiforgeryToken]
+[AuthorizeAdmin]
+[Area(AreaNames.Admin)]
+public class SeedProductSyncController : Controller
+{
+    // GET
+    public IActionResult Configure()
+    {
+        return View("~/Plugins/Seed.ProductSync/Views/Configure.cshtml");
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Seed.ProductSync/ProductSyncPlugin.cs
@@ -0,0 +1,20 @@
+﻿using Nop.Core;
+using Nop.Services.Plugins;
+using Task = System.Threading.Tasks.Task;
+
+namespace Nop.Plugin.Seed.ProductSync;
+
+public class ProductSyncPlugin:BasePlugin
+{
+    private readonly IWebHelper _webHelper;
+
+    public ProductSyncPlugin(IWebHelper webHelper)
+    {
+        _webHelper = webHelper;
+    }
+
+    public override string GetConfigurationPageUrl()
+    {
+        return $"{_webHelper.GetStoreLocation()}Admin/SeedProductSync/Configure";
+    }
+}
